c0b440df1c9d019eaa3a60e16dbf3da7c836c3a1
RKliaus <kliausroman@gmail.com>
2024-08-02 13:41:22 +0300

    homework 1 is done

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -5,10 +5,37 @@ const App = Vue.createApp({
             titleCalculator: 'Calculator',
             operator: '',
             countFirst: 0,
-            countSecond: 0
+            countSecond: 0,
+            countValue: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.countValue++
+        },
+        decrement() {
+            this.countValue--
+        },
+        setOperator(op) {
+            this.operator = op
+        }
+    },
+    computed: {
+        calculatedResult() {
+            switch(this.operator) {
+                case 'sum':
+                    return this.countFirst + this.countSecond
+                case 'subtract':
+                    return this.countFirst - this.countSecond
+                case 'multiply':
+                    return this.countFirst * this.countSecond
+                case 'divide':
+                    return this.countSecond !== 0 ? this.countFirst / this.countSecond : 'Error'
+                default:
+                    return 0
+            }   
         }
     }
 })
-
 
 App.mount('#app1')
