6f3554287e0c392c14e93b9c1faf85b8023b1bf6
Sepelite <lazyriser@gmail.com>
2024-08-04 20:13:58 +0300

    hw1 complete

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,13 +1,51 @@
 const App = Vue.createApp({
-    data() {
-        return {
-            titleCounter: 'Counter',
-            titleCalculator: 'Calculator',
-            operator: '',
-            countFirst: 0,
-            countSecond: 0
-        }
-    }
+	data() {
+		return {
+			titleCounter: 'Counter',
+			titleCalculator: 'Calculator',
+			counter: 0,
+
+			operator: '',
+			countFirst: 0,
+			countSecond: 0,
+		}
+	},
+
+	methods: {
+		increment() {
+			this.counter++
+		},
+		decrement() {
+			this.counter--
+		},
+
+		setOperator(operator) {
+			this.operator = operator
+		}
+	},
+
+	computed: {
+		result() {
+			let result = 0;
+
+			switch (this.operator) {
+				case 'sum':
+					result = this.countFirst + this.countSecond;
+					break;
+				case 'subtract':
+					result = this.countFirst - this.countSecond;
+					break;
+				case 'multiply':
+					result = this.countFirst * this.countSecond;
+					break;
+				case 'divide':
+					result = this.countFirst / this.countSecond;
+					break;
+			}
+
+			return result
+		}
+	}
 })
 
 
