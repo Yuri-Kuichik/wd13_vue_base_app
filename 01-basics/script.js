b73fc11cbd9f339deb632f0853f4160c220377c5
Bakhar Ilya <artemgray22@gmail.com>
2024-08-08 16:23:54 +0300

    HW 1 done

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -5,10 +5,28 @@ const App = Vue.createApp({
             titleCalculator: 'Calculator',
             operator: '',
             countFirst: 0,
-            countSecond: 0
+            countSecond: 0,
+            count: 0,
+        }
+    },
+
+    computed: {
+        calculation() {
+            if (this.operator === 'sum') {
+                return this.countFirst + this.countSecond;
+            }
+            if (this.operator === 'subtract') {
+                return this.countFirst - this.countSecond;
+            }
+            if (this.operator === 'multiply') {
+                return this.countFirst * this.countSecond;
+            }
+            if (this.operator === 'divide') {
+                return this.countFirst / this.countSecond;
+            }
+            return 0
         }
     }
 })
-
 
 App.mount('#app1')
