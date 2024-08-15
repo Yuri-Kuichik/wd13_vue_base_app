4d67eb28784d3810617133b715abb270964b383c
Sunwarts <yuradobrovolskij@mail.ru>
2024-08-15 15:31:25 +0300

    Done HW1

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,14 +1,32 @@
 const App = Vue.createApp({
     data() {
         return {
-            titleCounter: 'Counter',
+            titleCounter: 'Counter!',
             titleCalculator: 'Calculator',
-            operator: '',
+            operator: 'sum',
             countFirst: 0,
-            countSecond: 0
+            countSecond: 0,
+            count: 0
         }
-    }
+    }, 
+    computed: {
+    
+      result() {
+        if (this.operator === 'sum') {
+            return this.countFirst + this.countSecond
+        } else if (this.operator === 'subtract') {
+            return this.countFirst - this.countSecond
+        } else if (this.operator === 'multiply') {
+            return this.countFirst * this.countSecond
+        } else if (this.operator === 'divide') {
+            return this.countFirst / this.countSecond
+        } else {
+            return 0 
+        }
+      }
+}
 })
 
 
 App.mount('#app1')
+
